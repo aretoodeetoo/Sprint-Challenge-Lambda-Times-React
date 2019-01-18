27ae7717f39b0c86b49702212f62bb667895397e
croy <roy.christie95@gmail.com>
2019-01-18 12:40:53 -0600

    JK not the whole app, I missed a couple spots. Refactored CardContainer and Cards

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 import Card from './Card';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const CardsContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: none;
+  flex-direction: row;
+  width: 100%;
+  margin-top: 16px;
+  flex-wrap: wrap;
+
+  @media (min-width: 1200px){
+    width: 1200px;
+  }
+`
 
 const Cards = props => {
   return (
-    <div className="cards-container">
+    <CardsContainer>
       {props.cards.map(card => {
         return(
         <Card 
@@ -12,7 +27,7 @@ const Cards = props => {
         />
         )
       })}
-    </div>
+    </CardsContainer>
   )
 }
 
