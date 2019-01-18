6d487e62e7a16eb50f117099d607a18d9c036b6f
croy <roy.christie95@gmail.com>
2019-01-18 11:06:20 -0600

    Got cards to render

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
 const Tab = props => {
+  // if props.tab === props.selectedTab, className ='tab active-tab'
+  // else className='tab'
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
   return (
     <div
       className={''}
-      onClick={() => {
+      onClick={(tab) => {
+        {props.changeSelected}
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
       }}
